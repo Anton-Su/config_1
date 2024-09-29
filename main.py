f79a544ce5cd7e656df57963f2f03e80d02ee214
Anton-Su <suleykin2005@mail.ru>
2024-09-30 02:17:44 +0300

    update ls function

--- main.py
+++ main.py
@@ -19,24 +19,24 @@ def wc(path):
 
 def main():
     with ZipFile('archive.zip', 'a') as myzip:
-        path = ''
+        path = '/'
         while True:
             command = input('$ ')
             if command == 'exit':
                 break
             if command.startswith('ls'):
                 path_1 = path
+                flag = False
                 if len(command.split()) == 2:
                     path_1 = command.split()[1]
                 for file in myzip.namelist():
-                    if file.find(path_1) != -1 and file.count('/') + file.count('.') == path_1.count('/') + 1:
-                        example = file
-                        if (path_1):
-                            example = file.split(path_1)[1]
-                        print(example.strip('/'))
-                        #print(file.replace('/', "").split('/'))
-                    # if not file.find(path):
-                    #     print(file)
+                    if file.find(path_1) != -1:
+                        flag = True
+                        if file.count('/') + file.count('.') == path_1.count('/') + 1:
+                            example = file
+                            if (path_1):
+                                example = file.split(path_1)[1]
+                            print(example.strip('/'))
             elif command == 'cd':
                 pass
             elif command == 'touch':
