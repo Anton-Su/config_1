2bb7978f397d7b689d9752e8771f294059ebf507
Anton-Su <suleykin2005@mail.ru>
2024-09-29 23:00:14 +0300

    add base stubs

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def print_hi(name):
-    print(f'Hi44, {name}')
+def ls(path):
+    pass
+
+
+def cd(path):
+    pass
+
+
+def exi(path):
+    pass
+
+
+def touch(path):
+    pass
+
+
+def wc(path):
+    pass
+
+
+def main():
+    pass
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
