38fd8fc4d9af206c4e9f3419367a3bcccc6f6ef5
Anton-Su <suleykin2005@mail.ru>
2024-10-04 02:50:38 +0300

    исправлен скрипт

--- startscript.py
+++ startscript.py
@@ -1,1 +1,12 @@
-print(11111111)
+import main
+
+config = main.configparser.ConfigParser()
+config.read('configuration.ini')
+name = config.get('User', 'name')
+archivePath = config.get('ArchivePath', 'value')
+startScriptPath = config.get('StartScriptPath', 'value')
+
+
+main.ls(archivePath, '/')
+main.touch('touch privet.txt', '/', archivePath)
+main.wc('wc privet.txt', '/', archivePath)
