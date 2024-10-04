2f5da678e1a157e99f46b0ff0d36fa7a111dcdca
Anton-Su <suleykin2005@mail.ru>
2024-10-04 03:51:40 +0300

    введение первого корректного теста

--- tests/zip_tests.py
+++ tests/zip_tests.py
@@ -2,7 +2,7 @@ from unittest import TestCase, main
 import zip_program
 
 config = zip_program.configparser.ConfigParser()
-config.read('configuration.ini')
+config.read(r'C:\Users\Antua\PycharmProjects\config1\configuration.ini')
 name = config.get('User', 'name')
 archivePath = config.get('ArchivePath', 'value')
 startScriptPath = config.get('StartScriptPath', 'value')
@@ -10,8 +10,8 @@ startScriptPath = config.get('StartScriptPath', 'value')
 
 class Random(TestCase):
     def test_cd(self):
-        self.assertEquals(zip_program.cd('/'))
+        self.assertEqual(zip_program.cd(['cd', '123'], '/', archivePath), '/')
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
